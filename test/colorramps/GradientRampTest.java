efbeba3628cae3863f578c4b52e556bfd97f6d03
quantumjockey <nickyd242@gmail.com>
2014-11-05 18:14:06 -0800

    Created basic jUnit4 test to confirm functionality.

---
+++ test/colorramps/GradientRampTest.java
@@ -0,0 +1,29 @@
+package colorramps;
+
+import javafx.scene.paint.Color;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GradientRampTest {
+
+    Color[] stops;
+    GradientRamp ramp;
+
+    @Before
+    public void setUp() throws Exception {
+        stops = new Color[]{Color.BLUE, Color.RED, Color.YELLOW};
+        ramp = new GradientRamp(stops);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void GetRampColorValue() throws Exception {
+        Assert.assertEquals(Color.BLUE, ramp.getRampColorValue(0.0, 0.0, 0.5));
+    }
+}
